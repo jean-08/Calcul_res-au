991ff367fbd1ae15b0197700affbba776eb303be
kyami <167675352+jean-08@users.noreply.github.com>
2024-09-05 09:20:10 +0300

    Update_utils.h

--- utils.h
+++ utils.h
@@ -1,6 +1,6 @@
-
 void head();
 void get_data();
-char* compare_ip(int a, int b, int c, int d);
-char* msr_par_defaut(int a, int b, int c, int d);
+char *compare_ip(int a, int b, int c, int d);
+char *msr_par_defaut(int a, int b, int c, int d);
 void gestion_reseau(int a, int b, int c, int d);
+void calcul_adresses(int a, int b, int c, int d, const char *msr, char *reseau, char *broadcast);
